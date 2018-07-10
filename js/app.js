fda21672b09b7693c3abba43fdb710ac58a8d251
Eric <ericmiller@umn.edu>
2018-07-09 23:56:00 -0500

    created Player 'class' and mapped up the arrow keys to movement

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+	this.sprite = 'images/enemy-bug.png';
+	this.x = 1;
+	this.y = 2;
 };
 
 // Update the enemy's position, required method for game
@@ -25,11 +27,40 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+const Player = function() {
+	this.sprite = 'images/char-princess-girl.png';
+	this.x = 200;
+	this.y = 300;
+};
+
+Player.prototype.update = function(direction) {
+	if (direction === 'left') {
+		this.x -= 100;
+	}
+	else if (direction === 'right') {
+		this.x += 100;
+	}
+	else if (direction === 'up') {
+		this.y -= 83;
+	}
+	else if (direction === 'down') {
+		this.y += 83;
+	}
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+};
+
+Player.prototype.handleInput = function(event) {
+	this.update(event);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const allEnemies = [];
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
